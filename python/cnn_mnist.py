2c490a02d488e25ca3d6d23949560d11ec051fc7
odrzywolski-lukas <lukasellioto@gmail.com>
2018-02-26 10:33:43 -0500

    cnn_mnist.py
    cnn tutorial work

--- python/cnn_mnist.py
+++ python/cnn_mnist.py
@@ -71,8 +71,45 @@ def cnn_model_fn(feature, labels, mode):
 	eval_metric_ops = {
 		"accuracy": tf.metrics.accuracy(
 			labels=labels, predictions=predictions["classes"])}
-		return tf.estimator.EstimaorSpec(
-			mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
+	return tf.estimator.EstimaorSpec(
+		mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
+
+def main(usused_argv):
+	#load training and eval data 
+	mnist = tf.contrib.learn.datasets.load_dataset("mnist")
+	train_data = mnist.train.images 
+	train_labels = np.asarray(mnist.train.labels, dtype=np.int32)
+	eval_data = mnist.test.images
+	eval_labels = np.asarray(mnist.test.labels, dtype=np.int32)
+
+	mnist_classifier = tf.estimator.Estimator(
+		model_fn=cnn_model_fn, model_dir="/tmp/mnist_convnent_model")
+
+	#set up logging for predictions
+	tensors_to_log = {"probabilities": "softmax_tensor"}
+	logging_hook = tf.train.LoggingTensorHook(
+		tensors=tensors_to_log, every_n_inter=50)
+
+	#train the model
+	train_input_fn = tf.estimator.inputs.numpy_input_fn(
+		x={"x": train_data},
+		y=train_labels,
+		batch_size=100,
+		num_epochs=None,
+		shuffle=True)
+	mnist_classifier.train(
+		input_fn=train_input_fn,
+		steps=20000,
+		hooks=[logging_hook])
+
+	#evaluate the model and print results
+	eval_input_fn = tf.estimator.inputs.numpy_input_fn(
+		x={"x": eval_data},
+		y=eval_labels,
+		num_epochs=1,
+		shuffle=False)
+	eval_results = mnist_classifier.evaluate(input_fn=eval_input_fn)
+	print(eval_results)
 
 if __name__ == "__main__":
 	tf.app.run()
