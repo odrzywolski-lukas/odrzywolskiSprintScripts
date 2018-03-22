5fc57cb7ebfe91c7eecf8fe879d0d2c6504ae9e6
odrzywolski-lukas <lukasellioto@gmail.com>
2018-03-21 22:45:55 -0400

    sprint 3/20/18
    project euler:
    i have gotten problems 1,2,&4 working on the program
    i found a realy helpful github repository with a buch of these done, so i have used that as a reference when i get stuck
    
    problem 3 i havent started yet due to some complex library stuff.  im going to plan some stuff on that next

--- python/project_euler.py
+++ python/project_euler.py
@@ -1,8 +1,13 @@
-#Project Euler
+# Project Euler found here: https://projecteuler.net/archives
+# many problems can be found here:
+# https://github.com/nayuki/Project-Euler-solutions/tree/master/python
+# i plan on using this as a reference if i get stuck while working on a problem
 
 def main():
 	selections = {
 		"problem_1" : problem_1,
+		"problem_2" : problem_2,
+		"problem_4" : problem_4,
 		"termCMD" : False,
 		}
 
@@ -11,7 +16,7 @@ def main():
 	while True:
 		selection = userSelection()
 		if selections[selection]:
-			selections[selection] ()
+			print(selections[selection] ())
 		else:
 			print ("TERMINATING COMMAND LINE...")
 			break
@@ -24,6 +29,9 @@ def printHeader():
 
 eulerProblems = [
 	"problem_1",
+	"problem_2",
+	"problem_3",
+	"problem_4",
 	"termCMD",
 	]
 
@@ -36,9 +44,30 @@ def userSelection():
 
 def problem_1():
 	print("running problem 1")
-	print (sum(i for i in range(1000) if not (i % 3 and i % 5))) # this method of solving the problem works with the interface
+	ans = sum(i for i in range(1000) if not (i % 3 and i % 5)) # this method of solving the problem works with the interface
+	return str(ans)
 	# for i in range(1000):	#however this method does not
 	# 	if not i % 3 or not i % 5:
 	# 		yield i # when i changed this to return the entire function works, but the results are not printed
 	# print (sum(i))
+
+
+def problem_2():
+	print ("running problem 2")
+	ans = 0
+	x = 1
+	y = 2
+	while x <= 4000000:
+		if x % 2 == 0:
+			ans += x
+		x,y = y,x+y
+	return str(ans)
+
+def problem_4():
+	ans = max(i*j
+		for i in range(100,1000)
+		for j in range(100,1000)
+		if str(i*j) == str(i*j)[ : : -1])
+	return str(ans)
+
 main()
